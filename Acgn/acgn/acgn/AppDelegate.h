b9d67ac62d641df2ffecd9db5023363d050c6a2e
Ares.li <rymg2017@qq.com>
2018-01-31 18:41:33 +0800

    project

---
+++ Acgn/acgn/acgn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  acgn
+//
+//  Created by Ares on 2018/1/25.
+//  Copyright © 2018年 Jian LI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Acgn/acgn/acgn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  acgn
+//
+//  Created by Ares on 2018/1/25.
+//  Copyright © 2018年 Jian LI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
